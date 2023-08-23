135ae4ceadffb20dc482d419789e484ff4025c3a
VasileLin <vasile.linga1@gmail.com>
2023-08-23 14:29:49 +0300

    project init + add models

---
+++ DealershipAuto-Manager/DealershipAuto-Manager/Models/Sale.cs
@@ -0,0 +1,11 @@
+﻿namespace DealershipAuto_Manager.Models
+{
+    public class Sale
+    {
+        public Guid Id { get; set; }
+        public Car Car { get; set; }
+        public Client Client { get; set; }
+        public DateTime Date { get; set; }
+        public double FinalPrice { get; set; }
+    }
+}
