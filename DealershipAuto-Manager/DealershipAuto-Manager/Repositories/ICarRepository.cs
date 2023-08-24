d3b19a41c9bf14206484fe1036dd4c23bf31c802
VasileLin <vasile.linga1@gmail.com>
2023-08-24 16:03:54 +0300

    add car managemet logic

---
+++ DealershipAuto-Manager/DealershipAuto-Manager/Repositories/ICarRepository.cs
@@ -0,0 +1,13 @@
+﻿using DealershipAuto_Manager.Models;
+
+namespace DealershipAuto_Manager.Repositories
+{
+    public interface ICarRepository
+    {
+        void Add(Car car);
+        Car? Get(Guid id);
+        List<Car> GetAll();
+        void Update(Guid carId,Car car);
+        void Delete(Guid id);
+    }
+}
---
+++ DealershipAuto-Manager/DealershipAuto-Manager/Services/CarService.cs
@@ -0,0 +1,39 @@
+﻿using DealershipAuto_Manager.Models;
+using DealershipAuto_Manager.Repositories;
+
+namespace DealershipAuto_Manager.Services
+{
+    public class CarService : ICarService
+    {
+        private readonly ICarRepository _carRepository;
+
+        public CarService(ICarRepository carRepository)
+        {
+            _carRepository = carRepository;
+        }
+        public void Add(Car car)
+        {
+            _carRepository.Add(car);
+        }
+
+        public void Delete(Guid id)
+        {
+            _carRepository.Delete(id);
+        }
+
+        public Car? Get(Guid id)
+        {
+           return _carRepository.Get(id);
+        }
+
+        public List<Car> GetAll()
+        {
+           return _carRepository.GetAll();
+        }
+
+        public void Update(Guid carId, Car car)
+        {
+            _carRepository.Update(carId,car);
+        }
+    }
+}
