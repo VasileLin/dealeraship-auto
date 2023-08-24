3dbe5c0a525a077e9eda4c9ed5d48e73f79d645f
VasileLin <vasile.linga1@gmail.com>
2023-08-24 16:45:20 +0300

    add client management logic

---
+++ DealershipAuto-Manager/DealershipAuto-Manager/Services/IClientService.cs
@@ -0,0 +1,13 @@
+﻿using DealershipAuto_Manager.Models;
+
+namespace DealershipAuto_Manager.Services
+{
+    public interface IClientService
+    {
+        void Add(Client client);
+        Client? Get(Guid id);
+        List<Client> GetAll();
+        void Update(Guid clientId, Client client);
+        void Delete(Guid id);
+    }
+}
